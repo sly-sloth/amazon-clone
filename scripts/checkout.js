b2fd7f93e4fc8cbb76a9c918d9978320d0955869
Vaibhav <email.vaibhav.0@gmail.com>
2024-09-13 16:07:55 +0530

    created an oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderCheckoutHeader } from "./checkout/checkout-header.js";
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
+import '../data/cart-oop.js';
 
 
 renderCheckoutHeader();
