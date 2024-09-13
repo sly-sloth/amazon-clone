6bb3190caf4341a8ef0ad63f2c3136f888085228
Vaibhav <email.vaibhav.0@gmail.com>
2024-09-13 16:53:43 +0530

    added size chart to clothing

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCheckoutHeader } from "./checkout/checkout-header.js";
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
-import '../data/cart-class.js';
+// import '../data/cart-class.js';
 
 
 renderCheckoutHeader();
